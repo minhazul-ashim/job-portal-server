909e39cc5aca651cecc4f7d6eef6682d3ad40814
Ashraful <ashrafulislam2880@gmail.com>
2022-01-03 17:15:58 +0600

    Backend Added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Server-AI-Tours-Travels
+
+## Client Side Code Link: https://github.com/Ashraful2880/Client-AI-Tours---Travels
---
+++ index.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId=require('mongodb').ObjectId;
+require('dotenv').config();
+const app=express();
+const port=process.env.PORT || 5000;
+const cors=require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+
+//<------------- Database Code Here ---------->
+
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pxp8q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    async function run() {
+      try {
+        await client.connect();
+
+        //<------------Database All Collection ------------->
+ 
+        const database = client.db("Dev-Team-Job-Portal");
+        const allJobs = database.collection("AllJobs");
+
+        
+
+
+
+
+
+
+      } finally {
+        // await client.close();
+      }
+    }
+    run().catch(console.dir);
+    
+    app.get('/',(req,res)=>{
+      res.send('Running Dev-Team-Job-Portal')
+    });
+
+
+app.listen(port,()=>{
+    console.log("Running Server Port is",port);
+});
